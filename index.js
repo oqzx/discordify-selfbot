a9addd4d2ab0d189b938cd43efee17cfd7c204bb
Tejas Lamba <tejas22feb@gmail.com>
2024-05-16 17:26:18 +0530

    Subject: Refactor: Simplify require paths
    The codebase's require statements have been streamlined by removing the redundant './src' prefix from relative paths. This simplification improves readability and maintains consistency across the project.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 process.emitWarning = (warning, type, code, ctor) => type !== 'DeprecationWarning' || !warning.includes('punycode') && originalProcessEmitWarning(warning, type, code, ctor);
 
 require('dotenv').config();
-const { client } = require('./src/discord/client.js');
-const { playPlaylist } = require('./src/spotify/player.js');
-const logger = require('./src/logger/logger.js');
+const { client } = require('./discord/client.js');
+const { playPlaylist } = require('./spotify/player.js');
+const logger = require('./logger/logger.js');
 
 client.on('ready', () => {
     logger.info('Discord', `${client.user.username} | ID: ${client.user.id} connected to Discord.`);
